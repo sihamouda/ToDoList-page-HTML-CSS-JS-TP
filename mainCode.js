0ec70a09f9eaf863bc8296dd0b53f073734624bc
sihamouda <anis.c.hamouda@gmail.com>
2022-03-15 12:00:45 +0100

    js done some css issues maybe

--- mainCode.js
+++ mainCode.js
@@ -1,4 +1,4 @@
-var btn=document.querySelector("button");
+var btn=document.querySelector("#addNoteBtn");
 var noteSection=document.querySelector("#notes");
 
 btn.onclick= function (e){
@@ -6,12 +6,26 @@ btn.onclick= function (e){
     let name=document.querySelector("#name");
     let content=document.querySelector("#content");
 
-    let newNoteBox = document.createElement("p");
-    let newNote=document.createTextNode(name.value+":"+content.value)
+    var newNoteBox = document.createElement("div");
+    newNoteBox.className=("noteBox");
+    
+    let newDeleteBtn=document.createElement("button");
+    newDeleteBtn.textContent="🗑️";
+    
+    
+    let newNote = document.createElement("p");
+    let newNoteText=document.createTextNode(name.value+":"+content.value)
+    newNote.appendChild(newNoteText);
+    
     newNoteBox.appendChild(newNote);
+    newNoteBox.appendChild(newDeleteBtn);
     noteSection.appendChild(newNoteBox);
 
+    newDeleteBtn.onclick = function() {
+        newNoteBox.style.display = 'none';
+    }
 
-    console.log({name,content,newNoteBox})
+
+    console.log({name,content,newNote,newNoteBox,newDeleteBtn})
 
 }
