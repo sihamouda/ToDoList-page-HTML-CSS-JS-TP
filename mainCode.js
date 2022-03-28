d7642a49f1d389c5d1dbf418c7e4786a5088e4e9
sihamouda <anis.c.hamouda@gmail.com>
2022-03-28 17:11:04 +0100

    cleaner code + add bootstrap

--- mainCode.js
+++ mainCode.js
@@ -1,31 +1,31 @@
-var btn=document.querySelector("#addNoteBtn");
-var noteSection=document.querySelector("#notes");
+function addNote() {
+   let addBtn = document.querySelector("#addBtn");
+   let name = document.querySelector("#name");
+   let content = document.querySelector("#content");
+   let notes = document.querySelector("#notes");
 
-btn.onclick= function (e){
-    e.preventDefault();
-    let name=document.querySelector("#name");
-    let content=document.querySelector("#content");
+   let box = document.createElement("div");
+   box.className = "form-control";
 
-    var newNoteBox = document.createElement("div");
-    newNoteBox.className=("noteBox");
-    
-    let newDeleteBtn=document.createElement("button");
-    newDeleteBtn.textContent="🗑️";
-    
-    
-    let newNote = document.createElement("p");
-    let newNoteText=document.createTextNode(name.value+":"+content.value)
-    newNote.appendChild(newNoteText);
-    
-    newNoteBox.appendChild(newNote);
-    newNoteBox.appendChild(newDeleteBtn);
-    noteSection.appendChild(newNoteBox);
+   let row = document.createElement("div");
+   row.className = "row";
 
-    newDeleteBtn.onclick = function() {
-        newNoteBox.style.display = 'none';
-    }
+   let text = document.createElement("div");
+   text.className = "col-10";
+   text.appendChild(document.createTextNode(name.value + ":" + content.value));
 
+   let rmBtn = document.createElement("button");
+   rmBtn.className = "col-2 btn btn-primary";
+   rmBtn.textContent = "🗑️";
 
-    console.log({name,content,newNote,newNoteBox,newDeleteBtn})
+   row.appendChild(text);
+   row.appendChild(rmBtn);
+   box.appendChild(row);
+   notes.appendChild(box);
 
+   rmBtn.onclick = function () {
+      box.style.display = "none";
+   };
+
+   console.log({ addBtn, name, content, box, row, text, rmBtn });
 }
